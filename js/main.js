85f71684a93acbeeaf508da78f84a0e1fdb5f75c
Vishal Golhar <135511723+mrvishalg2004@users.noreply.github.com>
2024-05-11 00:35:10 +0530

    change the paths

--- js/main.js
+++ js/main.js
@@ -295,7 +295,7 @@ $(document).ready(function () {
 });
 
 // my background image code is start 
-var backgroundImages = ["url(images/compress/1.jpg)", "url(images/compress/3.jpg)", "url(images/compress/7.jpg)", "url(images/compress/4.jpg)"]; // Add paths to your images
+var backgroundImages = ["url(images/1.jpg)", "url(images/3.jpg)", "url(images/7.jpg)", "url(images/4.jpg)"]; // Add paths to your images
 var currentImageIndex = 0;
 
 function changeBackgroundImage() {
